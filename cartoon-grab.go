870eab24e005a639c25f13ac1809b6c9e6e74b36
Serhii Shobotov <sshobotov@gmail.com>
2014-10-31 16:32:37 +0200

    Added User-Agent header use for html downloading

--- cartoon-grab.go
+++ cartoon-grab.go
@@ -3,22 +3,25 @@ package main
 import (
     "flag"
     //"os"
+    "image"
     "fmt"
-    "bytes"
     //"io/ioutil"
     "net/http"
     "launchpad.net/xmlpath"
-	//"code.google.com/p/gofpdf"
+	"code.google.com/p/gofpdf"
 )
 
 var sourceUrl       = flag.String("u", "", "Source URL")
 var imageXPath      = flag.String("i", "", "Image xPath on the page")
 var nextPageXPath   = flag.String("l", "", "Next page link xPath on the page")
+const (
+    userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36"
+)
 
 func main() {
     flag.Parse()
 
-    if *sourceUrl == "" {
+    if *initialUrl == "" {
         fmt.Println("Use -u to setup source url")
         return
     }
@@ -30,13 +33,35 @@ func main() {
         fmt.Println("Use -l to setup next page link xPath")
         return
     }
+    pdf := gofpdf.New("P", "mm", "A4", "")
     client := &http.Client{}
+
+    makePage(client, pdf, *initialUrl);
+}
+
+func makePage(client *http.Client, pdf *gofpdf.Fpdf, pageUrl string) (success boolean) {
+    success = true
+
+    if imgUrl, nextUrl, ok := collect(client, pageUrl); ok {
+        add(pdf, imgUrl)
+        if nextUrl != "" {
+            makePage(client, pdf, nextUrl)
+        }
+    } else {
+        success = false
+    }
+    return
+}
+
+func collect(client *http.Client, url string) (imgUrl string, nextUrl string, success boolean) {
+    success = false
+
     req, err := http.NewRequest("GET", *sourceUrl, nil)
     if err != nil {
         fmt.Println(err)
         return
     }
-    req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
+    req.Header.Set("User-Agent", userAgent)
 
     response, err := client.Do(req)
     if err != nil {
@@ -54,12 +79,53 @@ func main() {
         fmt.Println(err)
         return
     }
-    buf := new(bytes.Buffer)
-    buf.ReadFrom(response.Body)
-    s := buf.String()
-    fmt.Println(s)
+
     path := xmlpath.MustCompile(*imageXPath + "/@src")
     if value, ok := path.String(root); ok {
-        fmt.Println("Found:", value)
+        imgUrl = value
     }
+    if (!ok) {
+        success = true
+        return
+    }
+    path := xmlpath.MustCompile(*nextPageXPath + "/@href")
+    if value, ok := path.String(root); ok {
+        nextUrl = value
+    }
+    return imgUrl, nextUrl, true
+}
+
+func add(client *http.Client, pdf *gofpdf.Fpdf, imgUrl string) (success string) {
+    success = false
+
+    req, err := http.NewRequest("GET", *sourceUrl, nil)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    req.Header.Set("User-Agent", userAgent)
+
+    response, err := client.Do(req)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer response.Body.Close()
+
+    m, _, err := image.Decode(resp.Body)
+    if err != nil {
+        fmt.Println(err)
+    }
+    g := m.Bounds()
+
+    height := g.Dy()
+    width := g.Dx()
+
+    if (height < width) {
+        pdf.AddPageFormat("L", nil)
+    } else {
+        pdf.AddPage()
+    }
+
+    
 }
