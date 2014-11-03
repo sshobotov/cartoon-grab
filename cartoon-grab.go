c80dfc335d67bc9451b43edc3bc319583b1b07b2
Serhii Shobotov <sshobotov@gmail.com>
2014-11-03 18:25:48 +0200

    Added correct validation messages

--- cartoon-grab.go
+++ cartoon-grab.go
@@ -4,6 +4,7 @@ import (
     "flag"
     "fmt"
     "strings"
+    "regexp"
     "net/http"
     "launchpad.net/xmlpath"
 	"code.google.com/p/gofpdf"
@@ -14,9 +15,13 @@ var (
     initialUrl      = flag.String("u", "", "Source URL")
     imageXPath      = flag.String("i", "", "Image xPath on the page")
     nextPageXPath   = flag.String("l", "", "Next page link xPath on the page")
+    forceOnImg404   = flag.Bool("f", false, "Force next urls processing if image request returns 404")
     baseUrl string
+    srcXPath *xmlpath.Path
+    hrefXPath *xmlpath.Path
 )
 const (
+    urlPattern      = "\\b(https?)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]"
     userAgent       = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36"
 )
 
@@ -26,55 +31,76 @@ func main() {
     flag.Parse()
 
     if *initialUrl == "" {
-        fmt.Println("Use -u to setup source url")
+        fmt.Println("Source URL should be provided")
+        return
+    }
+    r := regexp.MustCompile(urlPattern)
+    if !r.MatchString(*initialUrl) {
+        fmt.Println("Source URL string is not valid URI")
         return
     }
     urlParts := strings.SplitN(*initialUrl, "/", 4)
     baseUrl = urlParts[0] + "//" + urlParts[2]
 
     if *imageXPath == "" {
-        fmt.Println("Use -i to setup image xPath")
+        fmt.Println("Image src XPath should be provided")
+        return
+    }
+    var err error
+
+    srcXPath, err = xmlpath.Compile(*imageXPath)
+    if (err != nil) {
+        fmt.Println("Image src XPath is invalid XPath string")
         return
     }
     if *nextPageXPath == "" {
-        fmt.Println("Use -l to setup next page link xPath")
+        fmt.Println("Next page href XPath should be provided")
         return
     }
+    hrefXPath, err = xmlpath.Compile(*nextPageXPath)
+    if (err != nil) {
+        fmt.Println("Next page href XPath is invalid XPath string")
+        return
+    }
+
     pdf := gofpdf.New("P", "mm", "A4", "")
     client := &http.Client{}
 
     if ok := makePage(client, pdf, *initialUrl); ok {
-        pdf.OutputFileAndClose(uuid.New() + ".pdf")
-        fmt.Println("Done")
+        fileName := uuid.New() + ".pdf"
+        pdf.OutputFileAndClose(fileName)
+        fmt.Println("Done:", fileName)
     }
 }
 
 func makePage(client *http.Client, pdf *gofpdf.Fpdf, pageUrl string) (success bool) {
     success = true
 
-    if imgUrl, nextUrl, ok := collect(client, pageUrl); ok {
+    if imgUrl, nextUrl, ok := collectUrls(client, pageUrl); ok {
         if imgUrl == "" {
             return
         }
-        added := add(client, pdf, imgUrl)
-        if !added {
+        added := addContent(client, pdf, imgUrl)
+        if !added && !*forceOnImg404 {
             success = false
             return
         }
-        if nextUrl != "" {
-            if (strings.HasPrefix(nextUrl, "/")) {
-                nextUrl = baseUrl + nextUrl
-            }
-            fmt.Println("Next:", nextUrl)
-            success = makePage(client, pdf, nextUrl)
+        if nextUrl == "" {
+            return
         }
+        if strings.HasPrefix(nextUrl, "/") {
+            nextUrl = baseUrl + nextUrl
+        }
+        fmt.Println("Next:", nextUrl)
+        success = makePage(client, pdf, nextUrl)
     } else {
+        fmt.Println("Yep")
         success = false
     }
     return
 }
 
-func collect(client *http.Client, url string) (imgUrl string, nextUrl string, success bool) {
+func collectUrls(client *http.Client, url string) (imgUrl string, nextUrl string, success bool) {
     success = false
 
     req, err := http.NewRequest("GET", url, nil)
@@ -102,21 +128,19 @@ func collect(client *http.Client, url string) (imgUrl string, nextUrl string, su
         return
     }
 
-    srcPath := xmlpath.MustCompile(*imageXPath)
-    if value, ok := srcPath.String(root); ok {
+    if value, ok := srcXPath.String(root); ok {
         imgUrl = value
     } else {
         success = true
         return
     }
-    hrefPath := xmlpath.MustCompile(*nextPageXPath)
-    if value, ok := hrefPath.String(root); ok {
+    if value, ok := hrefXPath.String(root); ok {
         nextUrl = value
     }
     return imgUrl, nextUrl, true
 }
 
-func add(client *http.Client, pdf *gofpdf.Fpdf, imgUrl string) (success bool) {
+func addContent(client *http.Client, pdf *gofpdf.Fpdf, imgUrl string) (success bool) {
     success = false
 
     req, err := http.NewRequest("GET", imgUrl, nil)
@@ -134,6 +158,10 @@ func add(client *http.Client, pdf *gofpdf.Fpdf, imgUrl string) (success bool) {
     }
     defer response.Body.Close()
 
+    if response.StatusCode >= 400 {
+        fmt.Println("Unexpected image URL response status:", response.StatusCode)
+        return
+    }
     tp := pdf.ImageTypeFromMime(response.Header["Content-Type"][0])
     infoPtr := pdf.RegisterImageReader(imgUrl, tp, response.Body)
     if !pdf.Ok() {
